8bbfb9bd08d18d39b9b468e720b4112b9a342dc0
danyloday <danil.vladyka1@gmail.com>
2026-02-17 21:42:25 +0700

    Rate Us IOS

--- core/advertising/src/iosMain/kotlin/com.missclick.spy.core/advertising/RateUs.ios.kt
+++ core/advertising/src/iosMain/kotlin/com.missclick.spy.core/advertising/RateUs.ios.kt
@@ -1,8 +1,18 @@
 package com.missclick.spy.core.advertising
 
+import platform.StoreKit.SKStoreReviewController
+import platform.UIKit.UIApplication
+import platform.UIKit.UIWindowScene
+
 
 class RateUsIos(): RateUs  {
     override fun show() {
+        val scene = UIApplication.sharedApplication
+            .connectedScenes
+            .firstOrNull() as? UIWindowScene
 
+        if (scene != null) {
+            SKStoreReviewController.requestReviewInScene(scene)
+        }
     }
 }
